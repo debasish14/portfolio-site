be34d581e722f21f7320857e2daa612fdca932ae
debasish14 <120545552+debasish14@users.noreply.github.com>
2024-06-15 18:02:27 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -38,7 +38,7 @@ const skillsList = ["JavaScript", "React", "Python", "Node.js", "C", "Java"];
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation.";
 
 const About = () => {
   return (
